cdacc8c7901a364e27ff029543616e352770b7f0
Lucas Mihara <miharalucas@gmail.com>
2022-06-12 04:09:23 -0300

    added admin page and payment page

--- src/views/AdminPageView.vue
+++ src/views/AdminPageView.vue
@@ -1,3 +1,210 @@
 <template>
-  <div>a</div>
+  <div>
+    <div class="container background-light-light-blue my-5 p-6">
+      <h1 class="title is-2">Welcome back adminzao</h1>
+
+      <h2 class="collapse-header title is-3">Add new Card</h2>
+      <div class="background-white mx-5">
+        <form>
+          <div class="columns">
+            <div class="column px-5">
+              Card Name
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Card Name"
+                  name="cardName"
+                />
+              </div>
+              Price
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="Number"
+                  placeholder="10.00"
+                  min="0"
+                  name="cardPrice"
+                />
+              </div>
+              In Stock Quantity
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="Number"
+                  placeholder="10"
+                  min="0"
+                  step="1"
+                  name="cardQuantity"
+                />
+              </div>
+              Card Image
+              <div class="input-container">
+                <input
+                  class="input-field"
+                  type="file"
+                  name="cardImage"
+                />
+              </div>
+              <br>
+              <div class="input-container">
+                <button
+                  class="button is-primary"
+                  type="submit" 
+                  name="cardSubmit">
+                  Add card
+                </button>
+              </div>
+            </div>
+            <div class="column px-5">
+              Card Description
+              <div class="input-container">
+                <textarea
+                  class="textarea"
+                  type="text"
+                  placeholder="Description"
+                  name="cardDescription"
+                  value=""
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <br>
+      
+      <h2 class="collapse-header title is-3">Add new User</h2>
+      <div class="background-white mx-5">
+        <form>
+          <div class="columns">
+            <div class="column px-5">
+              <h3 class="title is-4">Personal Info</h3>
+              Name
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Username"
+                  name="name"
+                />
+              </div>
+              Email
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="email@provider.com"
+                  name="email"
+                />
+              </div>
+              Password
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="password"
+                  placeholder="Password"
+                  name="psw"
+                />
+              </div>
+              Confirm Password
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="password"
+                  placeholder="Confirm password"
+                  name="cpsw"
+                />
+              </div>
+              User Type
+              <div class="input-container">
+                <select class="select">
+                  <option value="customer">Customer</option>
+                  <option value="admin">Admin</option>
+                </select>
+              </div>
+              <br>
+              <button type="submit" class="button is-primary">Add User</button>
+            </div>
+
+            <div class="column px-5">
+              <h3 class="title is-4">Address</h3>
+
+              Postal Code
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="XXXXX-XXX"
+                  name="postcode"
+                />
+              </div>
+
+              Street
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Street"
+                  name="street"
+                />
+              </div>
+
+              Number
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="XX-XXx"
+                  name="num"
+                />
+              </div>
+
+              City
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="City"
+                  name="city"
+                />
+              </div>
+
+              State
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Country state"
+                  name="state"
+                />
+              </div>
+
+              Country
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Contry"
+                  name="country"
+                />
+              </div>
+
+              Phone
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="(DDD) XXXXX-XXXX"
+                  name="phone"
+                />
+              </div>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -1,3 +1,129 @@
 <template>
-  <div>a</div>
+  <div class="background-light-light-blue m-5 p-6 container">
+    <h1 class="title is-3">Payment informations</h1>
+    <form>
+      <section class="section">
+        Name printed in the card
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            placeholder="Name"
+            name="name"
+          />
+        </div>
+        <div class="columns">
+          <div class="input-container column is-three-fifths">
+            Card Number
+            <input
+              class="input"
+              type="text"
+              placeholder="Card Number"
+              name="cardNumber"
+            />
+          </div>
+          <div class="input-container column is-one-fifth">
+            Expire Date
+            <input
+              class="input"
+              type="text"
+              placeholder="MM/YY"
+              name="cardExpireDate"
+            />
+          </div>
+          <div class="input-container column is-one-fifth">
+            Security numbers
+            <input
+              class="input"
+              type="text"
+              placeholder="CVV"
+              name="cardCvv"
+            />
+          </div>
+        </div>
+      </section>
+      <h1 class="title is-3">Shipping Address</h1>
+      <section class="section">
+        Postal Code
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            placeholder="XXXXX-XXX"
+            name="postcode"
+          />
+        </div>
+
+        Street
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            placeholder="Street"
+            name="street"
+          />
+        </div>
+
+        Number
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            placeholder="XX-XXx"
+            name="num"
+          />
+        </div>
+
+        City
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            placeholder="City"
+            name="city"
+          />
+        </div>
+
+        State
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            placeholder="Country state"
+            name="state"
+          />
+        </div>
+
+        Country
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            placeholder="Contry"
+            name="country"
+          />
+        </div>
+      </section>
+      <section class="section">
+        <div class="tile is-5 background-light-blue py-5 px-0 is-vertical has-text-centered has-text-weight-semibold has-text-black">
+          <div class="columns mx-0">
+            <div class="column">
+              <p class="">Total:</p>
+              <p>Shipment Fee:</p>
+              <p>Discount:</p>
+              <p>Final price</p>
+            </div>
+            <div class="column">
+              <p class="">$ 6,66</p>
+              <p>$ 2,50</p>
+              <p>$ 0,00</p>
+              <p>$ 9,16</p>
+            </div>
+          </div>
+        </div>
+        <button class="button is-warning m-3">Return to cart</button>
+        <button type="submit" class="button is-primary m-3">Finish Transaction</button>
+      </section>
+    </form>
+  </div>
 </template>
