ef9cb2f23d7053b67dbbdc5f4a0076e0a0d1fca5
VitorBrust1 <vitorbrustolin2@hotmail.com>
2022-06-20 22:43:22 -0300

    Teste

---
+++ src/views/ChangeInfoView.vue
@@ -0,0 +1,170 @@
+<template>
+  <div>
+    <div class="sign-in-out">
+      <div class="sign-in">
+        <p class="title">
+          Change your profile information here, {{ userName ?? "Usuário" }}
+        </p>
+        <br /><br />
+        On this page you can change any of the informations of your profile,
+        just enter the new information on the side and press the button "Save"
+        at the end of the page! If you do not wish to change any data just leave
+        the spaces blank.
+
+        <img src="../assets/img/treinador.png" />
+      </div>
+      <div class="sign-up">
+        <p class="title">New profile information</p>
+        <br /><br />
+        <form class="form-sign-up" action="#">
+          New Username
+          <div class="input-container">
+            <i class="fa fa-user icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="Type your new username here"
+              name="name"
+            />
+          </div>
+          New Email
+          <div class="input-container">
+            <i class="fa fa-envelope icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="email@provider.com"
+              name="email"
+            />
+          </div>
+          Password
+          <div class="input-container">
+            <i class="fa fa-key icon"></i>
+            <input
+              class="input-field"
+              type="password"
+              placeholder="Type your new password here"
+              name="psw"
+            />
+          </div>
+          Confirm Password
+          <div class="input-container">
+            <i class="fa fa-key icon"></i>
+            <input
+              class="input-field"
+              type="password"
+              placeholder="Type your new password again here"
+              name="cpsw"
+            />
+          </div>
+
+          <br />
+          <h1>Address</h1>
+          <br />
+
+          Postal Code
+          <div class="input-container">
+            <i class="fas fa-envelope-open-text icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="XXXXX-XXX"
+              v-maska="'XXXXX-XXX'"
+              name="postcode"
+            />
+          </div>
+
+          Street
+          <div class="input-container">
+            <i class="fas fa-road icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="Type your new street name"
+              name="street"
+            />
+          </div>
+
+          Number
+          <div class="input-container">
+            <i class="fas fa-sort-numeric-down icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="XX-XXX"
+              v-maska="'XX-XXX'"
+              name="num"
+            />
+          </div>
+
+          City
+          <div class="input-container">
+            <i class="fas fa-city icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="Type the name of your new city"
+              name="city"
+            />
+          </div>
+
+          State
+          <div class="input-container">
+            <i class="fas fa-building icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="Type your new country state"
+              name="state"
+            />
+          </div>
+
+          Country
+          <div class="input-container">
+            <i class="fas fa-flag icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="Type your new contry name"
+              name="country"
+            />
+          </div>
+
+          Phone
+          <div class="input-container">
+            <i class="fas fa-phone icon"></i>
+            <input
+              class="input-field"
+              type="text"
+              placeholder="(DDD) XXXXX-XXXX"
+              v-maska="'(XX) XXXXX-XXXX'"
+              name="phone"
+            />
+          </div>
+
+          <button type="submit" class="btn" @click="edit_information()">
+            Change Informations
+          </button>
+        </form>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+<script>
+export default {
+  name: "SCC0219TrabalhoWebSignInUpView",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {
+    edit_information() {
+      alert("Voce mudou a informacao de perfil");
+    },
+  },
+};
+</script>
---
+++ src/views/EditCardView.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="container background-light-light-blue my-5 p-6">
+      <h1 class="title is-2">Welcome back adminzao</h1>
+      <h2 class="collapse-header title is-3">Edit a Card</h2>
+      <div class="background-white mx-5">
+        <form>
+          <div class="columns">
+            <div class="column px-5">
+              Card Name
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Card Name"
+                  name="cardName"
+                  required
+                />
+              </div>
+              Price of the card
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="Number"
+                  placeholder="10.00"
+                  min="0"
+                  name="cardPrice"
+                />
+              </div>
+              Stock Quantity
+              <div class="input-container">
+                <input
+                  class="input"
+                  type="Number"
+                  placeholder="10"
+                  min="0"
+                  step="1"
+                  name="cardQuantity"
+                  required
+                />
+              </div>
+              Card Image
+              <div class="input-container">
+                <input class="input-field" type="file" name="cardImage" />
+              </div>
+              <br />
+              <div class="input-container">
+                <button
+                  class="button is-primary"
+                  type="submit"
+                  name="cardSubmit"
+                >
+                  Edit card
+                </button>
+              </div>
+            </div>
+            <div class="column px-5">
+              Card Description
+              <div class="input-container">
+                <textarea
+                  class="textarea"
+                  type="text"
+                  placeholder="Description"
+                  name="cardDescription"
+                  value=""
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SCC0219TrabalhoWebSignInUpView",
+
+  data() {
+    return {
+      loginEmail: "",
+      loginPassword: "",
+    };
+  },
+
+  mounted() {},
+
+  methods: {
+    login() {
+      if (this.loginEmail == "" || this.loginPassword == "") {
+        alert("Suas informacoes foram trocadas com sucesso!");
+        this.$router.push({
+          name: "home",
+        });
+      }
+    },
+  },
+};
+</script>
